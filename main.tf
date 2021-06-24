c2e3d1bbd5c22b056d0ceb65b52b4c617c46619d
Jonatas Lopes <jlopsti@gmail.com>
2021-06-23 21:07:16 -0300

    suporte para aws-sso

--- main.tf
+++ main.tf
@@ -1,13 +1,54 @@
+################################################################################################
+#
+# Organizations Create Account
+#
 
 resource "aws_organizations_account" "main" {
-    count   = var.create ? length(var.create_account) : 0
+    count   = var.create ? 1 : 0
 
-    name        = var.create_account[count.index]["account_name"]
-    email       = var.create_account[count.index]["email"]
-    role_name   = lookup(var.create_account[count.index], "iam_role_name", null)
+    name        = var.account_name
+    email       = var.account_email
+    role_name   = var.assume_role_name
     
-    parent_id                   = lookup(var.create_account[count.index], "organizational_unit", null)
-    iam_user_access_to_billing  = lookup(var.create_account[count.index], "iam_user_access_to_billing", null)
+    parent_id                   = var.organizational_unit
+    iam_user_access_to_billing  = var.enable_user_access_to_billing
 
-    tags    = lookup(var.create_account[count.index], "default_tags", null)
+    tags    = var.default_tags
+}
+
+#
+# SSO Admins
+#
+
+data "aws_ssoadmin_instances" "main" {}
+data "aws_ssoadmin_permission_set" "main" {
+  count = var.create ? length(var.sso_permission_set_name) : 0
+  
+  instance_arn = tolist(data.aws_ssoadmin_instances.main.arns)[0]
+  name         = element(var.sso_permission_set_name, count.index)
+}
+
+data "aws_identitystore_group" "main" {
+  count = var.create ? length(var.sso_group_name) : 0
+
+  identity_store_id = tolist(data.aws_ssoadmin_instances.main.identity_store_ids)[0]
+  filter {
+    attribute_path  = "DisplayName"
+    attribute_value = element(var.sso_group_name, count.index)
+  }
+}
+
+resource "aws_ssoadmin_account_assignment" "main" {
+  count = var.create ? length(var.sso_permission_set_name) : 0
+
+  # Set PermissionSet on Account AWS
+  instance_arn       = element(data.aws_ssoadmin_permission_set.main.*.instance_arn, count.index)
+  permission_set_arn = element(data.aws_ssoadmin_permission_set.main.*.arn, count.index)
+
+  # Set Group on Account AWS
+  principal_id   = element(data.aws_identitystore_group.main.*.group_id, count.index)
+  principal_type = "GROUP"
+
+  target_id   = aws_organizations_account.main.0.id
+  target_type = "AWS_ACCOUNT"
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,37 @@ variable "create" {
     type    = bool
     default = true
 }
-variable "create_account" {
-    type    = any
+variable "account_name" {
+    description = "Nome da conta da AWS."
+    type    = string
+}
+variable "account_email" {
+    description = "Endereço de e-mail do proprietário da conta."
+    type    = string
+}
+variable "assume_role_name" {
+    description = "A conta de gerenciamento pode usar essa função do IAM para acessar recursos na conta-membro."
+    type    = string
+    default = "OrganizationAccountAccessRole"
+}
+variable "organizational_unit" {
+    type    = string
+    default = null
+}
+variable "enable_user_access_to_billing" {
+    type    = string
+    default = "DENY"
+}
+variable "sso_permission_set_name" {
+    type    = list
     default = []
 }
-
+variable "sso_group_name" {
+    type    = list
+    default = []
+}
+variable "default_tags" {
+    description = "As tags são pares de chave/valor que você pode adicionar aos recursos da AWS para ajudar a identificar, organizar e proteger seus recursos da AWS."
+    type    = map
+    default = {}
+}
